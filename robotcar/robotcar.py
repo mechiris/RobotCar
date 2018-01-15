18fd0af0b9b8dd78a033c3573b2657a96a44adf4
Ryan Smith <ryan@sightmachine.com>
2018-01-14 21:15:05 -0800

    initial robotcar commit.  event handler feedback works. Had to make blynk portion global, gave up on nesting within class.  If there is a better way to init I am all ears.

--- robotcar/robotcar.py
+++ robotcar/robotcar.py
@@ -3,28 +3,24 @@ import BlynkLib
 import time
 import math
 
+with open('creds.txt', 'r') as f:
+    BLYNK_AUTH = f.readline()
+# Initialize Blynk
+global blynk 
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
 class RobotCar():
+    def receiveXValue(self,value):
+            print('Current X value: ' + str(value))
+            value = int(value)
+            self.x = value
+            self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
 
-    @blynk.VIRTUAL_WRITE(1)
-    def my_write_handler(value):
-            print('Current V1 value: {}'.format(value))
-
-    @blynk.VIRTUAL_WRITE(2)
-    def my_write_handler(value):
-            print('Current V2 value: {}'.format(value))
-
-    @blynk.VIRTUAL_WRITE(3)
-    def my_write_handler(value):
-            print('Current V3 value: {}'.format(value))
-
-    def updateSpeedVectors(x=self.x,y=self.y):
-        self.x = x
-        self.y = y
-        self.r = math.sqrt(x^2 + y^2) / self.maxval^2
-
-
-    def start(self):
-        blynk.run()
+    def receiveYValue(self,value):
+            print('Current Y value: ' + str(value))
+            value = int(value)
+            self.y = value
+            self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
        
     def __init__(self):
         ## Assumes blynk joystick runs from -128->128 on x/y axis        
@@ -34,9 +30,15 @@ class RobotCar():
         self.y = 0
         self.r = 0
 
-        with open('creds.txt', 'r') as f:
-            self.BLYNK_AUTH = f.readline()
-        # Initialize Blynk
-        blynk = BlynkLib.Blynk(BLYNK_AUTH)
+rc = RobotCar()
 
+@blynk.VIRTUAL_WRITE(1)
+def my_write_handler(value):
+        rc.receiveXValue(value)
+        #print('Current V2 value: {}'.format(value))
 
+@blynk.VIRTUAL_WRITE(2)
+def my_write_handler(value):
+        rc.receiveYValue(value)
+
+blynk.run()
