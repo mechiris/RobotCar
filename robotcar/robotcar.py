2acf462e5299097602d756807d8e70b8e6c70550
Ryan Smith <ryan@sightmachine.com>
2018-01-14 21:34:34 -0800

    added motor logic.  untested

--- robotcar/robotcar.py
+++ robotcar/robotcar.py
@@ -2,6 +2,7 @@
 import BlynkLib
 import time
 import math
+from dual_g2_hpmd_rpi import motors, MAX_SPEED
 
 with open('creds.txt', 'r') as f:
     BLYNK_AUTH = f.readline()
@@ -21,10 +22,16 @@ class RobotCar():
             value = int(value)
             self.y = value
             self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
+
+    def updateMotorSpeeds(self):
+        motors.motor1.setSpeed(self.x)
+        motors.motor2.setSpeed(self.y)
        
     def __init__(self):
         ## Assumes blynk joystick runs from -128->128 on x/y axis        
         self.maxval = 128
+        motors.enable()
+
 
         self.x = 0
         self.y = 0
@@ -41,4 +48,8 @@ def my_write_handler(value):
 def my_write_handler(value):
         rc.receiveYValue(value)
 
-blynk.run()
+try:
+    blynk.run()
+finally:
+    motors.setSpeeds(0, 0)
+    motors.disable()
