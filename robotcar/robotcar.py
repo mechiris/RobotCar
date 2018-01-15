5a89ae8a67a7cca5125454601fd01b483c22670c
Ryan Smith <ryan@sightmachine.com>
2018-01-14 20:46:06 -0800

    fixed global function

--- robotcar/robotcar.py
+++ robotcar/robotcar.py
@@ -1,12 +1,7 @@
 
 import BlynkLib
 import time
-
-
-with open('creds.txt', 'r') as f:
-    self.BLYNK_AUTH = f.readline()
-# Initialize Blynk
-blynk = BlynkLib.Blynk(BLYNK_AUTH)
+import math
 
 class RobotCar():
 
@@ -22,8 +17,26 @@ class RobotCar():
     def my_write_handler(value):
             print('Current V3 value: {}'.format(value))
 
+    def updateSpeedVectors(x=self.x,y=self.y):
+        self.x = x
+        self.y = y
+        self.r = math.sqrt(x^2 + y^2) / self.maxval^2
+
+
     def start(self):
-
-
         blynk.run()
        
+    def __init__(self):
+        ## Assumes blynk joystick runs from -128->128 on x/y axis        
+        self.maxval = 128
+
+        self.x = 0
+        self.y = 0
+        self.r = 0
+
+        with open('creds.txt', 'r') as f:
+            self.BLYNK_AUTH = f.readline()
+        # Initialize Blynk
+        blynk = BlynkLib.Blynk(BLYNK_AUTH)
+
+
