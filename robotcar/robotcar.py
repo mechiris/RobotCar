b11b715655161b21378c8e6f149c9c6963164538
Ryan Smith <ryan@sightmachine.com>
2018-01-20 20:53:09 -0800

    added initial hookup from blynk to motor control (math isn't correct for driving yet)

--- robotcar/robotcar.py
+++ robotcar/robotcar.py
@@ -15,21 +15,22 @@ class RobotCar():
             print('Current X value: ' + str(value))
             value = int(value)
             self.x = value
-            self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
+            self.updateMotorSpeeds()
 
     def receiveYValue(self,value):
             print('Current Y value: ' + str(value))
             value = int(value)
             self.y = value
-            self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
+            self.updateMotorSpeeds()
 
     def updateMotorSpeeds(self):
+        self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
         motors.motor1.setSpeed(self.x)
         motors.motor2.setSpeed(self.y)
        
     def __init__(self):
         ## Assumes blynk joystick runs from -128->128 on x/y axis        
-        self.maxval = 128
+        #self.maxval = 128
         motors.enable()
 
 
