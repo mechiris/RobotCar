3f503fe9a86f3c001660268f35a01183b61e6bee
Ryan <ryan@ryanlsmith.com>
2018-08-12 03:24:34 +0000

    trueup from code on piø

--- robotcar/robotcar.py
+++ robotcar/robotcar.py
@@ -2,7 +2,10 @@
 import BlynkLib
 import time
 import math
-from dual_g2_hpmd_rpi import motors, MAX_SPEED
+import math
+from dual_g2_hpmd_rpi import motors#, MAX_SPEED
+
+MAX_SPEED=128
 
 with open('creds.txt', 'r') as f:
     BLYNK_AUTH = f.readline()
@@ -12,25 +15,51 @@ blynk = BlynkLib.Blynk(BLYNK_AUTH)
 
 class RobotCar():
     def receiveXValue(self,value):
-            print('Current X value: ' + str(value))
+            #print('Current X value: ' + str(value))
             value = int(value)
             self.x = value
             self.updateMotorSpeeds()
 
     def receiveYValue(self,value):
-            print('Current Y value: ' + str(value))
+            #print('Current Y value: ' + str(value))
             value = int(value)
             self.y = value
             self.updateMotorSpeeds()
 
     def updateMotorSpeeds(self):
-        self.r = math.sqrt(self.x**2 + self.y**2) / self.maxval**2
-        motors.motor1.setSpeed(self.x)
-        motors.motor2.setSpeed(self.y)
+        self.r = math.sqrt(self.x**2 + self.y**2) 
+        if self.r > 0:
+            theta = math.degrees(math.asin(self.x/self.r))
+        else:
+            theta = 0
+
+        if self.x >= 0:
+            if self.y > 0:
+                ## Quadrant 1
+                v_m1 = MAX_SPEED * self.r / self.maxval 
+                v_m2 = MAX_SPEED * self.y * self.r / self.maxval**2
+            else:
+                ## Quadrant 2
+                v_m1 = -1 * MAX_SPEED * self.r / self.maxval
+                v_m2 = MAX_SPEED * self.y * self.r / self.maxval**2
+        else:
+            if self.y > 0:
+                ## Quadrant 3
+                v_m2 = -1 * MAX_SPEED * self.r / self.maxval 
+                v_m1 = MAX_SPEED * self.y * self.r / self.maxval**2
+            else:
+                ## Quadrant 4
+                v_m2 = MAX_SPEED * self.r / self.maxval
+                v_m1 = MAX_SPEED * self.y * self.r / self.maxval**2
+        #v_m1 = int(MAX_SPEED *  theta * self.r/self.maxval)
+        #v_m2 = int(MAX_SPEED *  theta * self.r/self.maxval)
+        print ("velocities: " + str(v_m1) + ',' + str(v_m2) + ',' + str(theta))
+        motors.motor1.setSpeed(int(v_m1))
+        motors.motor2.setSpeed(int(v_m2))
        
     def __init__(self):
         ## Assumes blynk joystick runs from -128->128 on x/y axis        
-        #self.maxval = 128
+        self.maxval = 128
         motors.enable()
 
 
